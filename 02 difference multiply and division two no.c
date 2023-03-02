772e26e09cad64ccc6ce8fae070c2db0b85e28ef
bikash4044 <90145679+bikash4044@users.noreply.github.com>
2023-03-02 20:56:52 +0530

    Add files via upload

---
+++ 02 difference multiply and division two no.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    float a,b;
+    printf("Enter two no.:");
+    scanf("%f%f",&a,&b);
+    printf("\ndifference= %f",a-b);
+    printf("\nproduct= %f",a*b);
+    printf("\nquotient= %f",a/b);
+    return 0;
+}
